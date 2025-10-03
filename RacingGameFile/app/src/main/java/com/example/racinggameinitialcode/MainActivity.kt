4e8b844feaca5c837effaddce91af3e95f347eef
Conner Upton <connerupton05@gmail.com>
2025-10-03 10:43:52 -0400

    Basic Functioning Code That Runs
    This is the beginning of the code where this does NOTHING and just has compilation code that is only used for setting up the project

--- RacingGameFile/app/src/main/java/com/example/racinggameinitialcode/MainActivity.kt
+++ RacingGameFile/app/src/main/java/com/example/racinggameinitialcode/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.graphics.Point
 
 class MainActivity : AppCompatActivity() {
+    private var kotlinDrivingView: KotlinDrivingView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+            // Get a Display object to access screen details
+            val display = windowManager.defaultDisplay
+            // Load the resolution into a Point object
+            val size = Point()
+            display.getSize(size)
+
+            // Initialize gameView and set it as the view
+            kotlinDrivingView = KotlinDrivingView(this, size)   //NEED TO SET UP THE VIEW
+            setContentView(kotlinDrivingView)
     }
+
+    // This method executes when the player starts the game
+    override fun onResume() {
+        super.onResume()
+
+        // Tell the gameView resume method to execute
+        kotlinDrivingView?.resume()
+    }
+
+    // This method executes when the player quits the game
+    override fun onPause() {
+        super.onPause()
+
+        // Tell the gameView pause method to execute
+        kotlinDrivingView?.pause()
+        }
 }
